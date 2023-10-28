9a420a07b3b22da49171bedc698a12394a2beff9
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2023-10-28 10:48:47 -0700

    ad

--- client.py
+++ client.py
@@ -6,20 +6,19 @@ SERVER_HOST = '10.66.106.228'
 SERVER_PORT = 12345
 
 def main():
+    client_info = socket.gethostname()  
+
+    
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((SERVER_HOST, SERVER_PORT))
     while True:
  
             
-            client_info = socket.gethostname()  
+      
+    
 
             
-            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client_socket.connect((SERVER_HOST, SERVER_PORT))
-
-            
-            client_socket.send(client_info.encode())
-
-            
-            client_socket.close()
+        pass
 
             
    
