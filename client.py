e5db2de11ff39951b27819882990c9eb4a629bc0
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2023-10-28 10:43:35 -0700

    add

--- client.py
+++ client.py
@@ -1,23 +1,32 @@
 import socket
 import os
-
+import time
 
 SERVER_HOST = '10.66.106.228'
 SERVER_PORT = 12345
 
 def main():
-    
-    client_info = socket.gethostname()  
+    while True:
+        try:
+            
+            client_info = socket.gethostname()  
 
-    
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((SERVER_HOST, SERVER_PORT))
+            
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket.connect((SERVER_HOST, SERVER_PORT))
 
-    
-    client_socket.send(client_info.encode())
+            
+            client_socket.send(client_info.encode())
 
-    
-    client_socket.close()
+            
+            client_socket.close()
+
+            
+            time.sleep(60)  
+
+        except KeyboardInterrupt:
+            
+            break
 
 if __name__ == "__main__":
     main()
